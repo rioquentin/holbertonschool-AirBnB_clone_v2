0fd382f0e6836eb574b992c8420d4fdb886be0cc
guenoel <guenooel@hotmail.com>
2022-11-25 08:18:43 -0800

    modif relationship

--- models/city.py
+++ models/city.py
@@ -10,7 +10,7 @@ class City(BaseModel, Base):
     if getenv('HBNB_TYPE_STORAGE') == 'db':
         __tablename__ = 'cities'
         name = Column(String(128), nullable=False)
-        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+        state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
     else:
         state_id = ""
         name = ""
