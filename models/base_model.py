197cf912c72ffe83d0bbfd3b3d3df5d24bb7433d
guenoel <guenooel@hotmail.com>
2022-11-25 02:22:34 -0800

    __init__ basemodel done

--- models/base_model.py
+++ models/base_model.py
@@ -2,18 +2,28 @@
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
 from datetime import datetime
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, DateTime
+from os import getenv
 
 
+Base = declarative_base()
 class BaseModel:
     """A base class for all hbnb models"""
     def __init__(self, *args, **kwargs):
-        """Instatntiates a new model"""
+        """Instantiates a new model"""
         if not kwargs:
             from models import storage
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            storage.new(self)
+            if getenv('HBNB_TYPE_STORAGE') == 'db':
+                self.id = Column(String(60), nullable=False, primary_key=True)
+                self.created_at = Column(DateTime, nullable=False,
+                                         default=datetime.utcnow)
+                self.updated_at = Column(DateTime, nullable=False,
+                                         default=datetime.utcnow)
+            else:
+                self.id = str(uuid.uuid4())
+                self.created_at = datetime.now()
+                self.updated_at = datetime.now()
         else:
             kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
                                                      '%Y-%m-%dT%H:%M:%S.%f')
