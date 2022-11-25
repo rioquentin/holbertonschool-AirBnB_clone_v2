96edc9b7f2607f78a5f1b4a9cedd80c9781e8eeb
guenoel <guenooel@hotmail.com>
2022-11-25 07:18:48 -0800

    brouillon

--- models/state.py
+++ models/state.py
@@ -1,8 +1,32 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy.ext.declarative import relationship
+from sqlalchemy import ForeignKey, Column, String
+from models import storage
+from os import getenv
+from models.city import City
 
 
 class State(BaseModel):
     """ State class """
-    name = ""
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        __tablename__ = 'state'
+        name = Column(String(128), nullable=False)
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+        state = relationship(
+            "City",
+            backref='state',
+            cascade="delete",
+            )
+    else:
+        name = ""
+
+        @property
+        def cities(self):
+            "getter cities"
+            list_cities = []
+            for city in storage.all(City).values():
+                if city.state_id == self.id:
+                    list_cities.append(city)
+            return list_cities
