c9c218f65849384d31b75822a2ed10d07136b247
guenoel <guenooel@hotmail.com>
2022-11-25 07:16:46 -0800

    brouillon

--- models/city.py
+++ models/city.py
@@ -1,9 +1,21 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy.ext.declarative import relationship
+from sqlalchemy import ForeignKey, Column, String
+from models import type_storage
 
-
-class City(BaseModel):
+class City(BaseModel, Base):
     """ The city class, contains state ID and name """
-    state_id = ""
-    name = ""
+    if type_storage == 'db':
+        __tablename__ = 'cities'
+        name = Column(String(60), nullable=False)
+        state_id = Column(String(128), ForeignKey('states.id'), nullable=False)
+        places = relationship(
+            "Place",
+            backref='cities',
+            cascade="all, delete",
+            passive_deletes=True)
+    else:
+        state_id = ""
+        name = ""
