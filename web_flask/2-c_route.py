45e97c2dea792be9ac700fd619702b03ffd4591f
root <you@example.com>
2022-11-28 06:33:44 -0800

    Task 2 - Start a flask web app and add /c + print a variable

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Flask Web app starter"""
+
+
+from flask import Flask
+from markupsafe import escape
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_world():
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hello():
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def C(text):
+    new = text.replace("_", " ")
+    return f"C {escape(new)}"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
