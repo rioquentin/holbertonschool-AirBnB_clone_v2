6c513d428e3bab53332df237086427d02e694d66
root <you@example.com>
2022-11-28 06:36:13 -0800

    Task 2 - Start a flask web app and add /c + print a variable

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -7,16 +7,22 @@ from markupsafe import escape
 
 app = Flask(__name__)
 
+"""Base route"""
 
 @app.route("/", strict_slashes=False)
 def hello_world():
     return "Hello HBNB!"
 
+"""Route for /hbnb"""
 
 @app.route("/hbnb", strict_slashes=False)
 def hello():
     return "HBNB"
 
+"""
+Route for /c and display function of /c/<text> to print the value of the
+text variable
+"""
 
 @app.route("/c/<text>", strict_slashes=False)
 def C(text):
