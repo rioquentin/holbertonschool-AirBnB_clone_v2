bf4bf4cacee089b11b097391a66bb6a1f24fd213
root <you@example.com>
2022-11-29 00:58:43 -0800

    Task 4 - added route for int

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -45,12 +45,18 @@ text variable
 """
 
 
-@app.route("/python/", strict_slashes=False) #or {default='text': 'is cool'}
+@app.route("/python/", strict_slashes=False)
 @app.route("/python/<text>", strict_slashes=False)
 def Python(text="is cool"):
     """def"""
     new = text.replace("_", " ")
     return "Python {}".format(new)
+
+
+@app.route("/number/<int:number>", strict_slashes=False)
+def Num(number):
+    """def"""
+    return "{} is a number".format(number)
 
 
 if __name__ == '__main__':
